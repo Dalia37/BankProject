7dec7636f82f62433ad99f8567abe4c2369a5c66
Dalia Harazin <51988753+Dalia37@users.noreply.github.com>
2024-02-20 17:52:39 +0300

    Manager tests

--- src/TestCases/BankTests.java
+++ src/TestCases/BankTests.java
@@ -8,6 +8,7 @@ import static org.testng.Assert.assertEquals;
 import java.sql.Driver;
 import java.time.Duration;
 
+import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -18,10 +19,12 @@ import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 public class BankTests extends Parameters {
+	
+	
 	WebDriver driver = new ChromeDriver();
 	String URL = "https://globalsqa.com/angularJs-protractor/BankingProject/#/login";
 	String authID = "captchacharacters";
-	Parameters parameters = new Parameters();
+	
 	
 
 	@BeforeTest
@@ -33,21 +36,21 @@ public class BankTests extends Parameters {
 
 	}
 
-	@Test(priority = 1, enabled = true)
+	@Test(priority = 1, enabled = false)
 	public void CustomerLogin() {
 		WebElement btn = driver.findElement(By.cssSelector("div[class='borderM box padT20'] div:nth-child(1)"));
 		btn.click();
 
 	}
 
-	@Test(priority = 2, enabled = true)
+	@Test(priority = 2, enabled = false)
 	public void AnyRandomName() {
 		WebElement selector = driver.findElement(By.id("userSelect"));
 		selector.sendKeys(CustomersName[CustomerID]);
 
 	}
 
-	@Test(priority = 3, enabled = true)
+	@Test(priority = 3, enabled = false)
 	public void deposit() throws InterruptedException {
 		driver.findElement(By.className("btn-default")).click();
 		driver.findElement(By.cssSelector(".btn.btn-lg.tab[ng-class='btnClass2']")).click();
@@ -65,7 +68,7 @@ public class BankTests extends Parameters {
 
 	}
 
-	@Test(priority = 4, enabled = true)
+	@Test(priority = 4, enabled = false)
 	public void Withdrawl() throws InterruptedException {
 		driver.findElement(By.cssSelector(".btn.btn-lg.tab[ng-class='btnClass3']")).click();
 		Thread.sleep(5000);
@@ -85,15 +88,88 @@ public class BankTests extends Parameters {
 
 	}
 	
-	@Test(priority = 5, enabled = true)
+	@Test(priority = 5, enabled = false)
 	public void checkBalance() {
 		String ActualBalance = driver.findElement(By.xpath("//strong[normalize-space()='0']")).getText();
 	    String expexted = "0"; 
 	    assertEquals(ActualBalance, expexted);
-	    System.out.println("Suuuupppppppppppper");
+	  
 		
 	}
+	
+	
+	@Test(priority = 6 , enabled = true)
+	public void BankManagerLogin() {
+		WebElement ManagerLoginbtn = driver.findElement(By.cssSelector("button[ng-click='manager()']"));
+		ManagerLoginbtn.click();
+	}
+	@Test(priority = 7 , enabled =true)
+	public void AddOneCustomer() throws InterruptedException {
+		WebElement Addcustomerbtn = driver.findElement(By.cssSelector(".btn.btn-lg.tab[ng-class='btnClass1']"));
+		Addcustomerbtn.click();
+		
+		WebElement firstnameinput = driver.findElement(By.cssSelector("input[placeholder='First Name']"));
+		firstnameinput.sendKeys(firstNames[firstNameRandome]);
+		
+		WebElement lastNameinput = driver.findElement(By.cssSelector("input[placeholder='Last Name']"));
+		lastNameinput.sendKeys(lastNames[lastNameRandome]);
+		
+		WebElement postCodeinput = driver.findElement(By.cssSelector("input[placeholder='Post Code']"));
+		postCodeinput.sendKeys(postCode[postcodeRandom]);
+		
+		WebElement addcustomerbtn=driver.findElement(By.className("btn-default"));
+		addcustomerbtn.click();
+	    
+	
+		//test
+        Alert alert = driver.switchTo().alert();
+        String ActualText = alert.getText();
+        assertEquals(ActualText.contains("Customer added successfully"), true);
+        Thread.sleep(2000);
+        alert.accept();
+        
 
+
+	}
+	
+	@Test(priority = 8 , enabled = true)
+	public void openAccount() throws InterruptedException {
+		WebElement openbtn = driver.findElement(By.xpath("//button[normalize-space()='Open Account']"));
+		openbtn.click();
+		
+		WebElement customerinput = driver.findElement(By.id("userSelect"));
+		customerinput.sendKeys(CustomersName[CustomerID]);
+		
+		WebElement currencyinput = driver.findElement(By.id("currency"));
+		currencyinput.sendKeys(currency[carrencyRanom]);
+		
+		WebElement submit = driver.findElement(By.cssSelector("button[type='submit']"));
+		submit.click();
+		//Test
+		Alert alert = driver.switchTo().alert();
+		String ActualalertText =alert.getText();
+		assertEquals(ActualalertText.contains("Account created successfully"), true);
+		Thread.sleep(2000);
+		alert.accept();
+		
+		
+	}
+	@Test(priority = 9 , enabled = true)
+	public void deleteCustomer() {
+		WebElement Customerbtn = driver.findElement(By.xpath("//button[normalize-space()='Customers']"));
+		Customerbtn.click();
+		
+		WebElement container = driver.findElement(By.className("marTop"));
+		
+		int deletbtns = container.findElements(By.tagName("button")).size();
+		System.out.println(deletbtns);
+		
+
+	    container.findElements(By.tagName("button")).get(CustomerID).click();
+    	System.out.println("deleted");
+	
+	}
+	
 	@org.testng.annotations.AfterTest
 	public void AfterTest() {
 	}
--- src/TestCases/Parameters.java
+++ src/TestCases/Parameters.java
@@ -12,5 +12,24 @@ public class Parameters {
     Random rand = new Random();
     int CustomerID = rand.nextInt(CustomersName.length);
     int RandomAmount = MIN_AMOUNT + rand.nextInt(MAX_AMOUNT - MIN_AMOUNT + 1);
+    
+    String[] firstNames = {"Alice", "Bob", "Charlie", "David", "Eva", "Frank", "Grace", "Henry", "Ivy", "Jack"};
+    int firstNameRandome = rand.nextInt(firstNames.length);
+    
+    String[] lastNames = {"Smith", "Johnson", "Williams", "Jones", "Brown", "Davis", "Miller", "Wilson", "Moore", "Taylor"};
+    int lastNameRandome = rand.nextInt(lastNames.length);
+    
+   String[] postCode = {"12345","67890","54321","98765","23456","78901","34567","89012","45678","90123"};
+   int postcodeRandom = rand.nextInt(postCode.length);
+   
+   String[] currency = {"Dollar","Pound","Rupee"} ;
+   int carrencyRanom = rand.nextInt(currency.length);
+   
+
+
+
+
+    
+    
 	
 }
